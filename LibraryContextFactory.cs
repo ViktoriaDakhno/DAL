47d9e34614e46a6141cb5b04ddd2767e564bf9b6
Victoria Dakhno <8460185@stud.nau.edu.ua>
2025-04-08 21:59:39 +0300

    Додавання фабрики

---
+++ LibraryContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace LibraryApp.DAL
+{
+    public class LibraryContextFactory : IDesignTimeDbContextFactory<LibraryContext>
+    {
+        public LibraryContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<LibraryContext>();
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=LibraryDb;Trusted_Connection=True;Encrypt=False;TrustServerCertificate=True");
+
+            return new LibraryContext(optionsBuilder.Options);
+        }
+    }
+}
